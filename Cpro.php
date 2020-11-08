1bb47c5bfbdfe99fa893cf84317a4cb1d9d87e13
ali majidi <majidifarali76@gmail.com>
2020-11-07 23:33:13 -0800

    add php file

---
+++ Cpro.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'admin/model/Mpro.php';
+
+$pro = new pro();
+switch($action){
+    case 'list':
+        $result=$pro->pro_list();
+        break;
+    case 'detail':
+        $id=$_GET['id'];
+        $result=$pro->pro_showEdit($id);
+        break;
+}
+require_once 'view/'.$controller."/".$action.'.php';
